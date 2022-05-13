d6dc3480c2fd6dded7ddbe937a0a7af864c2d387
Noah Licata <noahinbldr@gmail.com>
2022-05-13 10:16:42 -0600

    add base html, css styling, some js scripting

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content was added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
